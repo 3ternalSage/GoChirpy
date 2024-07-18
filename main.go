9254cd33773d73f06e46200bd6e93ac81397877c
Aaditya Timalsina <aadityatimal@gmail.com>
2024-07-17 20:49:56 -0700

    custom handler

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 	"net/http"
 )
 
+type Handler interface {
+	ServeHTTP(http.ResponseWriter, *http.Request)
+}
+
 func main() {
 	var serverMux http.ServeMux = *http.NewServeMux()
 	var server http.Server
 
-	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+	serverMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+	serverMux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	server.Handler = &serverMux
 	server.Addr = "localhost:8080"
