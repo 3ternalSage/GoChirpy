6bfedbc9f75cf2836ed81f3f06ea44b8c7946191
Aaditya Timalsina <aadityatimal@gmail.com>
2024-07-17 17:08:36 -0700

    file server

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	var serverMux http.ServeMux = *http.NewServeMux()
 	var server http.Server
+
+	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server.Handler = &serverMux
 	server.Addr = "localhost:8080"
 	ok := server.ListenAndServe()
